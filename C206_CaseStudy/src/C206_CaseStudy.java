74e3804ad55abba6634132aeac66022c0bca3e52
SeanAwh <20041162@myrp.edu.sg>
2021-08-10 15:32:26 +0800

    Fixed delete cca

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class C206_CaseStudy {
 
 	
-
+//
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ArrayList<Student> studentList = new ArrayList<Student>();
